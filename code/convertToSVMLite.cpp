8cc204c1b672332a9b43ecfd21c715884ba6141f
Artur Abdullin <ar.abdullin@gmail.com>
2015-08-07 03:06:21 +0000

    Added support for C++ like indexe style. Index can start from 0

--- code/convertToSVMLite.cpp
+++ code/convertToSVMLite.cpp
@@ -12,7 +12,7 @@ typedef MSpMat::InnerIterator InIterMat;
 
 // convert sparse matrix to SVM lite format
 // [[Rcpp::export]]
-CharacterVector convertToSVMLite(MSpMat X, NumericVector labels = NumericVector::create()){
+CharacterVector convertToSVMLite(MSpMat X, NumericVector labels = NumericVector::create(), bool Cstyle = false){
      // number of columns
      int ncol = X.cols();
      int n_labels = labels.size();
@@ -20,25 +20,49 @@ CharacterVector convertToSVMLite(MSpMat X, NumericVector labels = NumericVector:
      // output vector
      CharacterVector out(ncol);
      
-     // for each column
-     for (int j = 0; j < ncol; j++) {
-        // start from empty string stream
-        std::ostringstream os;
-        
-        // add lebels if needed
-        if (n_labels > 0) {
-             int l = labels[j];
-             if (l == 0) l = -1;
-             os << l << " ";
-        };
-        
-        // for each non-zero element in the j column
-        for (InIterMat i_(X, j); i_; ++i_) {
-             os << i_.row() + 1 << ":" << i_.value() <<" ";
-        };
-        
-        out[j] = os.str();
-     }; // end j loop
+     if (Cstyle) { // col index will start from 0
+          // for each column
+          for (int j = 0; j < ncol; j++) {
+             // start from empty string stream
+             std::ostringstream os;
+             
+             // add lebels if needed
+             if (n_labels > 0) {
+                  int l = labels[j];
+                  if (l == 0) l = -1;
+                  os << l << " ";
+             };
+             
+             // for each non-zero element in the j column
+             for (InIterMat i_(X, j); i_; ++i_) {
+                  os << i_.row() << ":" << i_.value() <<" ";
+             };
+             
+             out[j] = os.str();
+          }; // end j loop
+          
+     } else { // col index will start from 1
+          
+          // for each column
+          for (int j = 0; j < ncol; j++) {
+             // start from empty string stream
+             std::ostringstream os;
+             
+             // add lebels if needed
+             if (n_labels > 0) {
+                  int l = labels[j];
+                  if (l == 0) l = -1;
+                  os << l << " ";
+             };
+             
+             // for each non-zero element in the j column
+             for (InIterMat i_(X, j); i_; ++i_) {
+                  os << i_.row() + 1 << ":" << i_.value() <<" ";
+             };
+             
+             out[j] = os.str();
+          }; // end j loop
+     }; // end of if statement
      
      return out;
 }
